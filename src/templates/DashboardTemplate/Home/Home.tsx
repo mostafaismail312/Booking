3c2d28f0656e62c34f4882a06d67f9a6a0f01a65
Ameera <amiraelalfe3@gmail.com>
2026-01-28 20:50:51 +0200

    dashboard

--- src/templates/DashboardTemplate/Home/Home.tsx
+++ src/templates/DashboardTemplate/Home/Home.tsx
@@ -1,7 +1,26 @@
+
 import React from 'react'
+import ChartsSquare from './ChartsSquare'
+import Charts from './Charts'
+import UsersCharts from './UsersCharts'
+import { Grid } from '@mui/material';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+    <ChartsSquare/>
+
+    <Grid container spacing={2} sx={{margin:20}}>
+    
+      <Grid size={6}>
+        <Charts />
+      </Grid>
+
+      
+      <Grid size={6}>
+        <UsersCharts />
+      </Grid>
+    </Grid>
+    </>
   )
 }
