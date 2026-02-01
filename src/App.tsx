b63d27d865b4fe2d8b326872d27e730e07e26c10
Ameera <amiraelalfe3@gmail.com>
2026-02-01 17:03:38 +0200

    mm

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ function App() {
           element: <VerifyAccount />,
         },
         {
-          path: PATHS.CHANGE_PASS_PATH,
+          path: "change-pass",
           element: <ChangePassword />,
         },
       ],
